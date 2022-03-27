b791655b505da3eff59103e2266b72033f860920
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2022-03-27 19:28:06 +0100

    Update mpu6050.py

--- robot/mpu6050.py
+++ robot/mpu6050.py
@@ -12,20 +12,14 @@ class mpu6050(Thread):
         i2c = board.I2C()  # uses board.SCL and board.SDA
         self.mpu = adafruit_mpu6050.MPU6050(i2c)
     
-        self.mpu_gyro = None
-        self.mpu_acceleration = None
+        self.gyro = None
+        self.acceleration = None
 
         self.poll = 0.01 # poll every <self.poll> seconds
 
-    def gyro(self):
-        return self.mpu_gyro
-
-    def acceleration(self):
-        return self.mpu_acceleration
-
     def run(self):
         while True:
-            self.mpu_gyro = self.mpu.gyro
-            self.mpu_acceleration = self.mpu.acceleration
+            self.gyro = self.mpu.gyro
+            self.acceleration = self.mpu.acceleration
 
             time.sleep(self.poll)
