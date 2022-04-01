54cd0b9dd8f4db2c1b5986f1a7dac23c170c4b91
Ivan Chan <ivanchan2000@hotmail.com>
2022-04-01 09:59:58 +0100

    Updated to conform to naming conventions Added documenation

--- src/robot/drive.py
+++ src/robot/drive.py
@@ -1,35 +1,36 @@
+"""Contains functionality for driving the robot"""
+
 import math
-import sys
-import time
 
-import ThunderBorg3 as ThunderBorg  # conversion for python 3
-
+from robot.accelerometer import perform_drive
 from robot.gyroscope import PerformSpin
-from robot.accelerometer import PerformDrive
 
-# Calculates the angle between a given vector and a known unit vector [up]
-# Returns a the angle needed to rotate from the known vector to match the given vector
+
 def calculate_angle(unit_target_vector):
-    # print("(" + str(x) + "," + str(y) + ")" + "(" + str(x_) + "," + str(y_) + ")")
+    """Calculates the angle between a given vector and a known unit vector [facing up]
+
+    Arguments:
+        unit_target_vector {tuple} -- a vector with a known unit vector [facing up]
+
+    Returns:
+        float -- the angle needed to rotate from the known vector to match the given vector
+    """
 
     origin_vector_x, origin_vector_y = (0, 1)
     target_vector_x, target_vector_y = unit_target_vector
 
-    # arc-cosine method does not produce signed output
-    #####
-    """
-    magnitude_origin = math.sqrt(origin_vector_x**2 + origin_vector_y**2)
-    magnitude_target = math.sqrt(target_vector_x**2 + target_vector_y**2)
+    # arc-cosine method does not produce signed output:
 
-    x_product = origin_vector_x * target_vector_x
-    y_product = origin_vector_y * target_vector_y
-    
-    product_sum = x_product + y_product
-    magnitude_product = magnitude_origin * magnitude_target
-
-    rad = math.acos( product_sum / magnitude_product  )
-    """
-    ######
+    # magnitude_origin = math.sqrt(origin_vector_x**2 + origin_vector_y**2)
+    # magnitude_target = math.sqrt(target_vector_x**2 + target_vector_y**2)
+    #
+    # x_product = origin_vector_x * target_vector_x
+    # y_product = origin_vector_y * target_vector_y
+    #
+    # product_sum = x_product + y_product
+    # magnitude_product = magnitude_origin * magnitude_target
+    #
+    # rad = math.acos( product_sum / magnitude_product  )
 
     rad = math.atan2(origin_vector_y, origin_vector_x) - math.atan2(
         target_vector_y, target_vector_x
@@ -42,6 +43,16 @@ def calculate_angle(unit_target_vector):
 
 
 def get_move_string(y, x):
+    """Converts a move to a string
+
+    Args:
+        y (int): the y-coordinate of the move
+        x (int): the x-coordinate of the move
+
+    Returns:
+        str: the string representation of the move
+    """
+
     vertical = {
         1: "up",
         -1: "down",
@@ -60,6 +71,18 @@ def get_move_string(y, x):
 
 
 def pathing(path, unit_size, origin=False, curr_angle=0):
+    """Drives the robot to the given path
+
+    Args:
+        path (list): a list of tuples representing the path
+        unit_size (int): the size of the unit the robot is moving in
+        origin (bool, optional): whether or not the robot is starting at the origin,\
+            defaults to False
+        curr_angle (int, optional): the angle the robot is starting at, defaults to 0
+
+    Returns:
+        list: a list of instructions to be performed by the robot
+    """
     if not origin:
         origin = path.pop(0)
 
@@ -84,7 +107,8 @@ def pathing(path, unit_size, origin=False, curr_angle=0):
         # move_right = (horizontal == 1)
         # move_left = (horizontal == -1)
 
-        # Use a unit vector to transform the next step to be relative to the previous point rather than to the origin
+        # Use a unit vector to transform the next step to be relative \
+        # to the previous point rather than to the origin
         unit_target_vector = (x_ - x, y - y_)
 
         target_angle = calculate_angle(unit_target_vector)
@@ -99,7 +123,7 @@ def pathing(path, unit_size, origin=False, curr_angle=0):
             curr_angle + delta_angle
         )  # update current angle to match the the robot's angle
 
-        instructions.append((PerformDrive, diagonal_unit_distance))
+        instructions.append((perform_drive, diagonal_unit_distance))
 
         print(get_move_string(vertical, horizontal))
         print("delta angle:" + str(delta_angle) + " global angle:" + str(target_angle))
@@ -109,5 +133,11 @@ def pathing(path, unit_size, origin=False, curr_angle=0):
 
 
 def follow(instructions):
+    """Follows a list of instructions
+
+    Args:
+        instructions (list): a list of instructions to be performed by the robot
+    """
+
     for action, arg in instructions:
         action(arg)
