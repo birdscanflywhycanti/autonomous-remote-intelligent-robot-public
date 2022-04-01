fab23562262858704e170028215e0c945b9b553b
Ivan Chan <ivanchan2000@hotmail.com>
2022-04-01 09:31:28 +0100

    Added Documentation for a_star.py

--- src/algorithms/a_star.py
+++ src/algorithms/a_star.py
@@ -1,9 +1,22 @@
+"""Contains the A* algorithm.
+"""
+
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 
 
 def a_star(matrix, start_node, end_node):
+    """A function that returns a path using the A* algorithm
+
+    Args:
+        matrix (list): a matrix representing the space the robot is in
+        start_node (tuple): the node the robot is starting from
+        end_node (tuple): the node the robot is trying to reach
+
+    Returns:
+        path (list) : a list of nodes representing the path the robot should take
+    """
     grid = Grid(matrix=matrix)
 
     start = grid.node(*start_node)
